83353b127fdc14e4bf30af9b589b0bda580c92a3
Leul Tefera <leulmulugeta1@gmail.com>
2021-08-19 08:59:09 -0500

    Added a custom JS with footer toggle

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// Footer links hider jQuery
+
+
+$(".footer-links-wrapper h3").click(function () {
+    if ($(window).width() <= 768) {
+      $(this).next("ul").slideToggle();
+      // $(this).parent().children("ul").slideToggle();
+      $(this).toggleClass("iconRotator");
+    }
+  });
+  
