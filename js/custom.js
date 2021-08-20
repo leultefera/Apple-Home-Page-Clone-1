6abad49f55905310e5ba5ce477d0aa336fbf8572
Leul Tefera <leulmulugeta1@gmail.com>
2021-08-19 19:26:00 -0500

    Fixed footer toggle and edited HTML content for footer

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,10 @@
 // Footer links hider jQuery
 
-
 $(".footer-links-wrapper h3").click(function () {
-    if ($(window).width() <= 768) {
-      $(this).next("ul").slideToggle();
-      // $(this).parent().children("ul").slideToggle();
-      $(this).toggleClass("iconRotator");
-    }
-  });
+  if ($(window).width() <= 768) {
+    $(this).next("ul").slideToggle();
+    // $(this).parent().children("ul").slideToggle();
+    $(this).toggleClass("iconRotator");
+  }
+});
   
